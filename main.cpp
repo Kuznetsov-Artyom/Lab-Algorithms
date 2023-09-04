f16c19a871e353a6b19377f4d5e8c914a54c1b62
Artyom <artyomka_working@mail.ru>
2023-09-04 21:47:22 +0300

    modificed class Timer

--- main.cpp
+++ main.cpp
@@ -4,10 +4,16 @@
 #include "headers/algorithms.hpp"
 #include "headers/timer.hpp"
 
+template <typename Period>
+void func() {
+  tmr::Timer<Period> timer;
+}
 
 int main(int argc, char* argv[], char** env) {
   std::vector<int> values{3, 5, 7, 2, 1, -2, 5, 7};
   tmr::Timer<tmr::nanosecond_t> timer;
+
+  func<tmr::second_t>();
 
   algs::quickSort(values, 0, values.size() - 1);
 
