8ed4e7e175a0eb7ec0dfa2a4d693b5d73087173c
Artyom <artyomka_working@mail.ru>
2023-09-04 21:10:34 +0300

    add first algorithm quickSort

---
+++ headers/algorithms.hpp
@@ -0,0 +1,31 @@
+#ifndef _ALGORITHMS_HPP_
+#define _ALGORITHMS_HPP_
+
+#include <vector>
+
+namespace algs {
+using pos_t = int64_t;
+
+template <typename T>
+void quickSort(std::vector<T>& arr, pos_t left, pos_t right) {
+  if (left > right) return;
+  int value = arr[(left + right) >> 1];
+  pos_t l = left;
+  pos_t r = right;
+
+  while (l <= r) {
+    while (arr[l] < value) ++l;
+    while (arr[r] > value) --r;
+
+    if (l <= r) {
+      std::swap(arr[l], arr[r]);
+      ++l;
+      --r;
+    }
+  }
+
+  quickSort(arr, left, r);
+  quickSort(arr, l, right);
+}
+}  // namespace algs
+#endif  // _ALGORITHMS_HPP_
--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <vector>
 
+#include "headers/algorithms.hpp"
 #include "headers/timer.hpp"
 
+
 int main(int argc, char* argv[], char** env) {
-  tmr::Timer<tmr::millisecond_t> timer;
+  std::vector<int> values{3, 5, 7, 2, 1, -2, 5, 7};
+  tmr::Timer<tmr::nanosecond_t> timer;
 
-  std::vector<int> values;
-
-  for (int i = 0; i < 1'000'000; ++i) values.push_back(i + 1);
+  algs::quickSort(values, 0, values.size() - 1);
 
   std::cout << timer() << '\n';
 
