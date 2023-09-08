6f9b8311ae649df55b8c7ef73371160b7b7f9482
Artyom <artyomka_working@mail.ru>
2023-09-08 20:01:13 +0300

    add tests

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 #include "algorithms.hpp"
 #include "timer.hpp"
 
-
 template <typename T, template <typename Elem, typename = std::allocator<Elem>>
                       class Container>
 std::ostream& operator<<(std::ostream& out, const Container<T>& cont) {
@@ -30,23 +29,45 @@ static void testAvlSort() {
 static void testQuickSort() {
   std::cout << __FUNCTION__ << "()\n";
 
-  std::vector<int> elems{5, 2, 1, -100, INT_MAX,1,1,1,1, 12, 0, 32, 90};
+  std::vector<int> elems{5, 2, 1, -100, INT_MAX, 1, 1, 1, 1, 12, 0, 32, 90};
 
   std::cout << "before: " << elems << '\n';
   algs::quickSort(elems, 0, elems.size() - 1);
   std::cout << "after: " << elems << '\n';
 }
 
-static void testAvlTreeLong(std::vector<int> elems) {
-  TIMER_START(timer, tmr::millisecond_t);
-  algs::avlSort(elems);
-  std::cout << TIMER_GET(timer) << '\n';
+static void testAvlTreeLong(std::vector<int>& elems, size_t countTest) {
+  std::cout << __FUNCTION__ << "()\n";
+
+  long long total = 0;
+
+  for (size_t i = 0; i < countTest; ++i) {
+    std::vector<int> arr(elems);
+
+    TIMER_START(timer, tmr::millisecond_t);
+    algs::avlSort(elems);
+    total += TIMER_GET(timer);
+  }
+
+  std::cout << "tests:\t\t" << countTest << '\n';
+  std::cout << "avg time:\t" << total / countTest << " ms\n";
 }
 
-static void testQuickSortLong(std::vector<int> elems) {
-  TIMER_START(timer, tmr::millisecond_t);
-  algs::quickSort(elems, 0, elems.size() - 1);
-  std::cout << TIMER_GET(timer) << '\n';
+static void testQuickSortLong(std::vector<int>& elems, size_t countTest) {
+  std::cout << __FUNCTION__ << "()\n";
+
+  long long total = 0;
+
+  for (size_t i = 0; i < countTest; ++i) {
+    std::vector<int> arr(elems);
+
+    TIMER_START(timer, tmr::millisecond_t);
+    algs::quickSort(elems, 0, elems.size() - 1);
+    total += TIMER_GET(timer);
+  }
+  
+  std::cout << "tests:\t\t" << countTest << '\n';
+  std::cout << "avg time:\t" << total / countTest << " ms\n";
 }
 
 int main(int argc, char* argv[], char** env) {
@@ -59,8 +80,10 @@ int main(int argc, char* argv[], char** env) {
   std::vector<int> elems(1'000'000);
   for (auto& elem : elems) elem = gen();
 
-  testAvlTreeLong(elems);
-  testQuickSortLong(elems);
+  size_t countTest = 10;
+
+  testAvlTreeLong(elems, countTest);
+  testQuickSortLong(elems, countTest);
 
   return 0;
 }
